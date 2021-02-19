95b197f5f79890d8ba8311490ae3d1fdd9c356ef
YahiaNagib <yahianagib123@gmail.com>
2021-02-19 18:07:08 +0200

    edit styles

--- network/models.py
+++ network/models.py
@@ -2,11 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from time import strftime
+from PIL import Image
 
 class User(AbstractUser):
     followings = models.ManyToManyField("self", related_name="followers", symmetrical=False, blank=True)
     join_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 class Post(models.Model):
